ab41292132d34eb2d73a7efc1fdef57853f80722
shsssskn <shsssskn@gmail.com>
2019-03-12 17:46:55 +0900

    [fix] b-15 toCamel, toSnake 再帰処理

--- src/array.js
+++ src/array.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import Case from 'case';
 
-export const getDetailById = (id, data, idFormat?) => {
+export const getDetailById = (id, data, idFormat) => {
   const key = idFormat ? idFormat : 'id';
   const targetId = Number(id);
 
@@ -56,33 +56,23 @@ export const checkValueInOIA = (key, value, array) => {
 };
 
 export const toCamel = collection => {
-  if (_.isArray(collection)) {
-    const results = collection.map(value => {
-      const shaped = _.mapKeys(value, (v, k) => Case.camel(k));
-      return shaped;
-    });
-    return results;
-  } else if (_.isPlainObject(collection)) {
-    return _.mapKeys(collection, (v, k) => Case.camel(k));
-  } else if (_.isString(collection)) {
-    return Case.camel(collection);
-  }
-
-  // ArrayまたはObject、Stringで無ければそのまま
-  return collection;
+  return __changeCases(collection, 'camel');
 };
 
 export const toSnake = collection => {
+  return __changeCases(collection, 'snake');
+};
+
+const __changeCases = (collection, caseName) => {
   if (_.isArray(collection)) {
-    const results = collection.map(value => {
-      const shaped = _.mapKeys(value, (v, k) => Case.snake(k));
-      return shaped;
-    });
-    return results;
+    return collection.map(value => __changeCases(value, caseName));
   } else if (_.isPlainObject(collection)) {
-    return _.mapKeys(collection, (v, k) => Case.snake(k));
+    let obj = {...collection};
+    obj = _.mapKeys(obj, (v, k) => Case[caseName](k));
+    obj = _.mapValues(obj, v => __changeCases(v, caseName));
+    return obj;
   } else if (_.isString(collection)) {
-    return Case.snake(collection);
+    return Case[caseName](collection);
   }
 
   // ArrayまたはObject、Stringで無ければそのまま
