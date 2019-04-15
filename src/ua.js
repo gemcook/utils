7131dbc76c92f6aa8359b6cb98bb9db79dc008a8
Takuya Fujimoto <t.fujimoto@gemcook.com>
2019-04-15 12:38:49 +0900

    [update] ua

--- src/ua.js
+++ src/ua.js
@@ -5,3 +5,17 @@ export const isSp = () => {
   const result = os === 'Android' || os === 'iOS' || os === 'Windows Phone';
   return result;
 };
+
+export const isAndroid = () => {
+  const os = platform.os.family;
+  return os === 'Android';
+};
+
+export const isIOS = () => {
+  const os = platform.os.family;
+  return os === 'iOS';
+};
+
+export const get = () => {
+  return platform;
+};
